a74bde764be828f10c369a6f643bb9d35fcd4fca
justintjoa <justintoja@gmail.com>
2021-02-18 23:36:20 -0800

    box is now edge only

--- Tagoose-IOS-Plugin/UI/Components/ScanSceneView.swift
+++ Tagoose-IOS-Plugin/UI/Components/ScanSceneView.swift
@@ -18,10 +18,24 @@ class Scanner {
         let config = ARObjectScanningConfiguration()
         sceneView.session.run(config, options: .resetTracking)
         
-        let box:SCNNode = SCNNode(geometry: SCNBox(width: CGFloat(0.1), height: CGFloat(0.1), length: CGFloat(0.1), chamferRadius: CGFloat(0)))
-        box.geometry?.firstMaterial?.fillMode = .lines
-        box.geometry?.firstMaterial?.diffuse.contents = UIColor.green
-        sceneView.scene.rootNode.addChildNode(box)
+        let sm = "float u = _surface.diffuseTexcoord.x; \n" +
+            "float v = _surface.diffuseTexcoord.y; \n" +
+            "int u100 = int(u * 100); \n" +
+            "int v100 = int(v * 100); \n" +
+            "if (u100 % 99 == 0 || v100 % 99 == 0) { \n" +
+            "  // do nothing \n" +
+            "} else { \n" +
+            "    discard_fragment(); \n" +
+            "} \n"
+        
+        let box = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+
+        box.firstMaterial?.emission.contents = UIColor.green
+
+        box.firstMaterial?.shaderModifiers = [SCNShaderModifierEntryPoint.surface: sm]
+
+        box.firstMaterial?.isDoubleSided = true
+        sceneView.scene.rootNode.addChildNode(SCNNode(geometry: box))
     }
     
 
